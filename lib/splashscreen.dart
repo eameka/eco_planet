29ef3b9fe5e009fd777c2208fc88ebea23893e86
Ameka <edetatj@gmail.com>
2024-05-05 08:13:28 +0000

    Second commit

---
+++ lib/splashscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+
+class Mysplash extends StatefulWidget {
+  const Mysplash({
+    super.key,
+  });
+
+  @override
+  State<Mysplash> createState() => _MysplashState();
+}
+
+class _MysplashState extends State<Mysplash> {
+  @override
+  Widget build(BuildContext context) {
+    return const Stack();
+    }
+}
